22137b3c198e9fe4cfdc39b8dbdb98c2facd6bbb
Gustavo Di Salvo <gustavo_disalvo@yahoo.com>
2021-11-03 20:35:27 -0300

    Migrated version - just run
    Drop GitHub for Unity.
    Migrated to Unity 2021.2
    Some cleaning to begin refactoring

--- Assets/Karting/Scripts/Utilities/CineMachineTargeteer.cs
+++ Assets/Karting/Scripts/Utilities/CineMachineTargeteer.cs
@@ -1,4 +1,4 @@
-﻿using Cinemachine;
+﻿// using Cinemachine;
 using UnityEngine;
 
 namespace KartGame.Utilities
@@ -6,7 +6,7 @@ namespace KartGame.Utilities
     /// <summary>
     /// A preview utility to help cycle the Cinemachine Camera to look at and follow various targets.
     /// </summary>
-    [RequireComponent(typeof(CinemachineVirtualCamera))]
+    // [RequireComponent(typeof(CinemachineVirtualCamera))]
     public class CineMachineTargeteer : MonoBehaviour
     {
         [Tooltip("What gameObjects do we want to look at?")]
@@ -14,13 +14,13 @@ namespace KartGame.Utilities
         [Tooltip("Which key allows us to switch targets? By default, we use the Spacebar.")]
         public KeyCode CyclingKey = KeyCode.Space;
 
-        CinemachineVirtualCamera m_VirtualCam;
+        // CinemachineVirtualCamera m_VirtualCam;
         int m_Index;
 
         void Start()
         {
-            m_VirtualCam = GetComponent<CinemachineVirtualCamera>();
-            m_Index = 0;
+            // m_VirtualCam = GetComponent<CinemachineVirtualCamera>();
+            // m_Index = 0;
         }
 
         void Update()
@@ -29,11 +29,11 @@ namespace KartGame.Utilities
             {
                 m_Index             = (m_Index + 1) % Targets.Length;
                 Transform target    = Targets[m_Index];
-                m_VirtualCam.Follow = m_VirtualCam.LookAt = target;
+                // m_VirtualCam.Follow = m_VirtualCam.LookAt = target;
 
 #if UNITY_EDITOR
                 // By pinging the gameObject we can easily find the gameObject in the Hierarchy view.
-                UnityEditor.EditorGUIUtility.PingObject(m_VirtualCam.Follow);
+                // UnityEditor.EditorGUIUtility.PingObject(m_VirtualCam.Follow);
 #endif
             }
         }
